b25e20011843ffaa43dda8d1d32dac8f283a81a7
Kamil Sienkiewicz <sienkiewiczkm@gmail.com>
2017-10-31 22:06:59 +0100

    FIX: Increased precision of intermediate framebuffer used to store Monte Carlo partial result

--- source/Application.cpp
+++ source/Application.cpp
@@ -99,7 +99,7 @@ void Application::onCreate()
 
         glGenTextures(1, &_intermediateFBOTexture);
         glBindTexture(GL_TEXTURE_2D, _intermediateFBOTexture);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, resolution.x, resolution.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, resolution.x, resolution.y, 0, GL_RGBA, GL_FLOAT, nullptr);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, _intermediateFBOTexture, 0);
