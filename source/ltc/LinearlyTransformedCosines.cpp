efade7172fbd3c389ed78bb3230d1850102015d1
Kamil Sienkiewicz <sienkiewiczkm@gmail.com>
2017-11-05 22:45:09 +0100

    FIX: Full linear interpolation for LTC lookups

--- source/ltc/LinearlyTransformedCosines.cpp
+++ source/ltc/LinearlyTransformedCosines.cpp
@@ -113,8 +113,8 @@ void LinearlyTransformedCosines::loadLookupTextures()
     }
 
     glBindTexture(GL_TEXTURE_2D, ltcMatB);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
